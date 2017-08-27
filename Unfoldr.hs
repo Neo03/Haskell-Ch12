fdb53492fcffa8a8ab27cf85a740eefa3f9fefbd
Василий Симаков <vasiliy@sim.papa>
2017-08-27 14:04:03 +0300

    Added Unfoldr

---
+++ Unfoldr.hs
@@ -0,0 +1,13 @@
+module Unfoldr where
+
+myIterate :: (a -> a) -> a -> [a]
+myIterate f a = a : myIterate f (f a)
+
+
+myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+myUnfoldr f b = case f b of
+  Nothing -> []
+  Just (x, y) -> x : myUnfoldr f y
+
+betterIterate :: (a -> a) -> a -> [a]
+betterIterate f  = myUnfoldr (\y -> Just (y, f y))    

245f799c188af0c1f3a53991bd9f5007812a8853
Василий Симаков <vasiliy@sim.papa>
2017-08-27 21:57:27 +0300

    Added Trees

--- Trees.hs
+++ Trees.hs
@@ -1,1 +1,18 @@
 module Trees where
+
+data BinaryTree a =
+   Leaf
+  |Node (BinaryTree a) a (BinaryTree a)
+  deriving (Ord, Eq, Show)
+
+unfold :: (a -> Maybe(a, b, a)) -> a -> BinaryTree b
+unfold f b = case f b of
+                Nothing -> Leaf
+                Just (x, y, z) -> Node (unfold f x) y (unfold f z)
+
+
+treeBuild :: Integer -> BinaryTree Integer
+treeBuild n = unfold f 0
+  where f m
+          | m == n = Nothing
+          | otherwise = Just (m+1, m , m+1)
